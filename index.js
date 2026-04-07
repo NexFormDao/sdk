8392dae5ce7d189f3cbe3e3439826900571f2b54
NexForm <haihhshhs87@gmail.com>
2026-04-07 23:38:58 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+```javascript
+const express = require("express");
+const app = express();
+const apiRoutes = require("./routes/api");
+
+app.use(express.json());
+app.use("/api", apiRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+```
