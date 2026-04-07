39342f414295d0ab1508b9345930ee586cf0debd
NexForm <haihhshhs87@gmail.com>
2026-04-07 23:38:43 +0700

    Create deploy.js

---
+++ deploy.js
@@ -0,0 +1,17 @@
+```javascript
+const hre = require("hardhat");
+
+async function main() {
+  const NexForm = await hre.ethers.getContractFactory("NexForm");
+  const nexform = await NexForm.deploy();
+
+  await nexform.deployed();
+
+  console.log("NexForm deployed to:", nexform.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+```
